5f43d936e1cdc9287f7f9c4f1a86e98540107651
yuwu <wuyumail>
2018-08-22 16:18:00 +0800

    init

---
+++ common/src/main/java/org/geometerplus/fbreader/fbreader/ActionCode.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2007-2017 FBReader.ORG Limited <contact@fbreader.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+package org.geometerplus.fbreader.fbreader;
+
+public class ActionCode {
+	public static final String SHOW_LIBRARY = "library";
+	public static final String SHOW_PREFERENCES = "preferences";
+	public static final String SHOW_BOOK_INFO = "bookInfo";
+	public static final String SHOW_TOC = "toc";
+	public static final String SHOW_BOOKMARKS = "bookmarks";
+	public static final String SHOW_NETWORK_LIBRARY = "networkLibrary";
+
+	public static final String SWITCH_TO_NIGHT_PROFILE = "night";
+	public static final String SWITCH_TO_DAY_PROFILE = "day";
+
+	public static final String SHARE_BOOK = "shareBook";
+
+	public static final String SEARCH = "search";
+	public static final String FIND_PREVIOUS = "findPrevious";
+	public static final String FIND_NEXT = "findNext";
+	public static final String CLEAR_FIND_RESULTS = "clearFindResults";
+
+	public static final String SET_TEXT_VIEW_MODE_VISIT_HYPERLINKS = "hyperlinksOnlyMode";
+	public static final String SET_TEXT_VIEW_MODE_VISIT_ALL_WORDS = "dictionaryMode";
+
+	public static final String TURN_PAGE_BACK = "previousPage";
+	public static final String TURN_PAGE_FORWARD = "nextPage";
+
+	public static final String MOVE_CURSOR_UP = "moveCursorUp";
+	public static final String MOVE_CURSOR_DOWN = "moveCursorDown";
+	public static final String MOVE_CURSOR_LEFT = "moveCursorLeft";
+	public static final String MOVE_CURSOR_RIGHT = "moveCursorRight";
+
+	public static final String VOLUME_KEY_SCROLL_FORWARD = "volumeKeyScrollForward";
+	public static final String VOLUME_KEY_SCROLL_BACK = "volumeKeyScrollBackward";
+	public static final String SHOW_MENU = "menu";
+	public static final String SHOW_NAVIGATION = "navigate";
+
+	public static final String GO_BACK = "goBack";
+	public static final String EXIT = "exit";
+	public static final String SHOW_CANCEL_MENU = "cancelMenu";
+
+	public static final String SET_SCREEN_ORIENTATION_SYSTEM = "screenOrientationSystem";
+	public static final String SET_SCREEN_ORIENTATION_SENSOR = "screenOrientationSensor";
+	public static final String SET_SCREEN_ORIENTATION_PORTRAIT = "screenOrientationPortrait";
+	public static final String SET_SCREEN_ORIENTATION_LANDSCAPE = "screenOrientationLandscape";
+	public static final String SET_SCREEN_ORIENTATION_REVERSE_PORTRAIT = "screenOrientationReversePortrait";
+	public static final String SET_SCREEN_ORIENTATION_REVERSE_LANDSCAPE = "screenOrientationReverseLandscape";
+
+	public static final String INCREASE_FONT = "increaseFont";
+	public static final String DECREASE_FONT = "decreaseFont";
+
+	public static final String DISPLAY_BOOK_POPUP = "displayBookPopup";
+	public static final String PROCESS_HYPERLINK = "processHyperlink";
+
+	public static final String SELECTION_SHOW_PANEL = "selectionShowPanel";
+	public static final String SELECTION_HIDE_PANEL = "selectionHidePanel";
+	public static final String SELECTION_CLEAR = "selectionClear";
+	public static final String SELECTION_COPY_TO_CLIPBOARD = "selectionCopyToClipboard";
+	public static final String SELECTION_SHARE = "selectionShare";
+	public static final String SELECTION_TRANSLATE = "selectionTranslate";
+	public static final String SELECTION_BOOKMARK = "selectionBookmark";
+
+	public static final String OPEN_VIDEO = "video";
+
+	public static final String HIDE_TOAST = "hideToast";
+	public static final String OPEN_START_SCREEN = "openStartScreen";
+	public static final String OPEN_WEB_HELP = "help";
+	public static final String INSTALL_PLUGINS = "plugins";
+}
